fc36016fcfb28c0a704b6bdbdc3589d4b34ba042
Mauro Bono <bono.mauro@gmail.com>
2018-03-22 12:28:10 +0100

    create app structure with create-react-app, add main component and some style

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    search() {
+        console.log('this.state = ', this.state);
+    }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-title">Music Master from App</div>
+                {/* built using react-bootstrap */}
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl
+                            type="text"
+                            placeholder="Search for an Artist"
+                            value={this.state.query}
+                            onChange={event => {this.setState({query: event.target.value})}}
+                            onKeyPress={event => {
+                                if(event.key === 'Enter') {
+                                    this.search();
+                                }
+                            }}
+                        />
+                        <InputGroup.Addon onClick={() => this.search()}>
+                            <Glyphicon glyph="search">
+                            </Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+                <div className="Profile">
+                    <div>Artist Picture</div>
+                    <div>Artist Name</div>
+                </div>
+                <div className="Gallery">
+                Gallery
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
